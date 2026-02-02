72c64238523bcaeff5f2aa9da2922d1eb3dc7ca7
Noa Galvez <komasan17tv@gmail.com>
2026-02-02 15:51:41 +0100

    feat: add BackupState and BackupType enums for backup job management

---
+++ src/EasySave.Core/Enums/BackupType.cs
@@ -0,0 +1,17 @@
+namespace EasySave.Core.Enums
+{
+    /// <summary>
+    /// Enum representing the type of backup.
+    /// </summary>
+    public enum BackupType
+    {
+        /// <summary>
+        /// Represents a full backup.
+        /// </summary>
+        Full,
+        /// <summary>
+        /// Represents an Differential backup.
+        /// </summary>
+        Differential
+    }
+}
