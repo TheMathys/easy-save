804d7b77b299d64783b0673223c7f7a88edf33d9
Roquette86 <estebarillot@gmail.com>
2026-02-02 16:32:10 +0100

    feat: implementing BackupJob class

---
+++ src/EasySave.Core/Entities/BackupJob.cs
@@ -0,0 +1,12 @@
+﻿using EasySave.Core.Enums;
+
+namespace EasySave.Core.Entities;
+
+public class BackupJob
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = ""; // Name of the backup
+    public string SourcePath { get; set; } = ""; // Source of the file
+    public string TargetPath { get; set; } = ""; // Path to the saved file 
+    public BackupType Type { get; set; } // Type of backup
+}
