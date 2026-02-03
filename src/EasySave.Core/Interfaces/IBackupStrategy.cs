78f61ae0caf1257baca05b18e805979968e0c61e
Esteban <estebarillot@gmail.com>
2026-02-03 10:22:54 +0100

    feat: add interfaces IBackupStrategy and IBackupStrategyFactory

---
+++ src/EasySave.Core/Interfaces/IBackupStrategy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using EasySave.Core.Entities;
+using EasySave.Core.Models;
+
+namespace EasySave.Core.Interfaces
+{
+    /// <summary>
+    /// Defines the filtering of eligible files (complete vs differential)
+    /// </summary>
+    public interface IBackupStrategy
+    {
+        /// <summary>
+        /// Get the eligible files according to the backup type (complete or differential)
+        /// </summary>
+        /// <param name="job"></param>
+        /// <param name="files"></param>
+        /// <param name="differentialSinceUtc"></param>
+        /// <param name="ct"></param>
+        /// <returns></returns>
+        public IAsyncEnumerable<FileItem> GetEligibleFilesAsync(BackupJob job, IAsyncEnumerable<FileItem> files,
+            DateTime? differentialSinceUtc, CancellationToken ct);
+    }
+}
---
+++ src/EasySave.Core/Interfaces/IBackupStrategyFactory.cs
@@ -0,0 +1,17 @@
+﻿using EasySave.Core.Enums;
+
+namespace EasySave.Core.Interfaces
+{
+    /// <summary>
+    /// Returns the strategy according to the type
+    /// </summary>
+    public interface IBackupStrategyFactory
+    {
+        /// <summary>
+        /// Give the strategy according to the backup type
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public IBackupStrategy GetStrategy(BackupType type);
+    }
+}
