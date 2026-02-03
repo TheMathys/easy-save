e21098114cbaa2000f1b827d071e7722975a009b
faritietl <faritietl@gitlab.lycee-vieljeux.fr>
2026-02-03 10:32:26 +0100

    fix:LogEntry comments corrected

--- src/EasyLog/LogEntry.cs
+++ src/EasyLog/LogEntry.cs
@@ -12,17 +12,46 @@ namespace EasyLog
     /// </summary>
     public class LogEntry
     {
-        public DateTime TimeStamp { get; } // Date and time of the log entry
-        public string BackupName { get; } // Name of the backup job
-        public string SourcePath { get; } // Source path of the backup
-        public string DestinationPath { get; } // Destination path of the backup
-        public long FileSizeBytes { get; } // Size of the file transferred in bytes
-        public TimeSpan TrasnferTimeMs { get; } // Time taken to transfer the file
+        /// <summary>
+        /// Date and time of the log entry.
+        /// </summary>
+        public DateTime TimeStamp { get; } 
 
         /// <summary>
-        /// Initializes a new instance of the LogEntry class.
+        /// Name of the backup job.
         /// </summary>
-        public LogEntry(DateTime timeStamp, string backupName, string sourcePath, string destinationPath,long fileSizeBytes, TimeSpan transferTimeMs)
+        public string BackupName { get; } 
+
+        /// <summary>
+        /// Source path of the backup.
+        /// </summary>
+        public string SourcePath { get; } 
+
+        /// <summary>
+        /// Destination path of the backup.
+        /// </summary>
+        public string DestinationPath { get; } 
+
+        /// <summary>
+        /// Size of the file transferred in bytes.
+        /// </summary>
+        public long FileSizeBytes { get; } 
+
+        /// <summary>
+        /// Time taken to transfer the file.
+        /// </summary>
+        public TimeSpan TrasnferTimeMs { get; } 
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LogEntry"/> class.
+        /// </summary>
+        /// <param name="timeStamp">Date and time of the log entry.</param>
+        /// <param name="backupName">Name of the backup job.</param>
+        /// <param name="sourcePath">Source path of the backup.</param>
+        /// <param name="destinationPath">Destination path of the backup.</param>
+        /// <param name="fileSizeBytes">Size of the file transferred in bytes.</param>
+        /// <param name="transferTimeMs">Time taken to transfer the file.</param>
+        public LogEntry(DateTime timeStamp, string backupName, string sourcePath, string destinationPath, long fileSizeBytes, TimeSpan transferTimeMs)
         {
             TimeStamp = timeStamp;
             BackupName = backupName;
