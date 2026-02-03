53aa085517c24e1ead3b6fe25f57e7001f1c33b2
faritietl <faritietl@gitlab.lycee-vieljeux.fr>
2026-02-03 10:56:49 +0100

    feat add ILogWriter for EasyLog

---
+++ src/EasyLog/ILogWriter.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace EasyLog
+{
+    /// <summary>
+    /// Internal contract for a log writer.
+    /// Implementations write a log entry asynchronously.
+    /// </summary>
+    internal interface ILogWriter
+    {
+        /// <summary>
+        /// Writes a log entry asynchronously.
+        /// </summary>
+        /// <param name="logEntry">The log entry to write.</param>
+        /// <param name="cancellationToken">Cancellation token to cancel the operation if required.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
+        Task WriteAsync(LogEntry logEntry, CancellationToken cancellationToken);
+    }
+}
