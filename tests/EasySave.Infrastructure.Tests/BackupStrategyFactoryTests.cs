2292fa84d50e015ae87280e11363b32f65a37f48
faritietl <faritietl@gitlab.lycee-vieljeux.fr>
2026-02-04 20:51:32 +0100

    test: Creating test file for BackupStrategyFactory - Can be completed by another teamate

---
+++ tests/EasySave.Infrastructure.Tests/BackupStrategyFactoryTests.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using EasySave.Core.Interfaces;
+using EasySave.Core.Models;
+using EasySave.Infrastructure.Backup;
+using EasySave.Infrastructure.Tests;
+using Xunit;
+
+namespace EasySave.Infrastructure.Tests
+
+
+    /**
+     * 
+     * #TODO: Implement tests bellow to check the validity of BackupStrategyFactory
+     *
+     **/
+{
+    private readonly FullBackupStrategyTests _fullTests;
+    private readonly DifferentialBackupStrategyTests _differentialTests;
+    public sealed class BackupStrategyFactoryTests
+    {
+
+    }
+}
