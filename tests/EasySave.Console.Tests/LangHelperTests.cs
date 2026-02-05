27213cb066e29bc07c54e7a80409c1192ab5c66c
Esteban <estebarillot@gmail.com>
2026-02-05 14:18:21 +0100

    feat: add resources strings (#107)

---
+++ tests/EasySave.Console.Tests/LangHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using System.Globalization;
+using System.Reflection;
+using System.Threading;
+using EasySave.Console.Resources;
+using Xunit;
+
+namespace EasySave.Tests
+{
+    public class LangHelperTests
+    {
+        private CultureInfo _originalCulture;
+        private CultureInfo _originalUICulture;
+
+        public LangHelperTests()
+        {
+            _originalCulture = Thread.CurrentThread.CurrentCulture;
+            _originalUICulture = Thread.CurrentThread.CurrentUICulture;
+        }
+
+        [Fact]
+        public void GetString_ExistingKey_ReturnsCorrectString()
+        {
+            var result = LangHelper.GetString("BackupCancel");
+
+            Assert.NotNull(result);
+            Assert.Contains("Cancel the backup.", result); 
+        }
+
+        [Fact]
+        public void GetString_NonExistingKey_ReturnsNull()
+        {
+            var result = LangHelper.GetString("NonExistentKey");
+
+            Assert.Null(result);
+        }
+
+        [Fact]
+        public void GetString_NullKey_ReturnsNull()
+        {
+            Assert.Null(LangHelper.GetString(null));
+        }
+    }
+}
