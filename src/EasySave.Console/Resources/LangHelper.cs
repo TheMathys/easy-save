eef58cf88e9e9ac349dbb7524a2e553f88beea78
Esteban <estebarillot@gmail.com>
2026-02-05 11:43:25 +0100

    feat: add LangHelper, to manage the different languages

---
+++ src/EasySave.Console/Resources/LangHelper.cs
@@ -0,0 +1,36 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Globalization;
+using System.Threading;
+
+namespace EasySave.Console.Resources
+{
+    /// <summary>
+    /// This class handles language management.
+    /// </summary>
+    public class LangHelper
+    {
+        private static ResourceManager _rm;
+
+        static LangHelper()
+        {
+            _rm = new ResourceManager("ConsoleApp1.Resources.Strings", Assembly.GetExecutingAssembly());
+        }
+
+        public static string? GetString(string name)
+        {
+            return _rm.GetString(name);
+        }
+
+        public static void ChangeLanguage(string lang)
+        {
+            var cultureInfo = new CultureInfo(lang);
+        
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+    
+            CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+            CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+        }
+    }   
+}
